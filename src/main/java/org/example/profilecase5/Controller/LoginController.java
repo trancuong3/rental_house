96e4d09c347b8d4bea7ad5ab169dfc29f9f2bbd8
Your Namechipkiet <you@example hoccuakiet@gmail.com>
2024-11-12 21:50:45 +0700

    update account

---
+++ src/main/java/org/example/profilecase5/Controller/LoginController.java
@@ -0,0 +1,16 @@
+package org.example.profilecase5.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping
+    public String listings(Model model) {
+        return "login/login";
+    }
+}
