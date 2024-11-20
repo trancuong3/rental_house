d60477d17ce8e48cb33ad5bc2cc20638eae7612f
cuong <you@example.com>
2024-11-20 19:02:54 +0700

    demo

--- src/main/java/org/example/profilecase5/Service/UserService.java
+++ src/main/java/org/example/profilecase5/Service/UserService.java
@@ -114,7 +114,11 @@ public class UserService {
     public void toggleUserStatus(int userId) {
         User user = userRepository.findById(userId).orElse(null);
         if (user != null) {
-            user.setStatus(user.getStatus() == User.Status.ACTIVE ? User.Status.LOCKED : User.Status.ACTIVE);
+            if (user.getStatus() == User.Status.ACTIVE) {
+                user.setStatus(User.Status.LOCKED);
+            } else {
+                user.setStatus(User.Status.ACTIVE);
+            }
             userRepository.save(user);
         }
     }
